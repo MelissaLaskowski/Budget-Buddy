3625636eff420a148dd18608abc2ff4286209963
MelissaLaskowski <mxl7583@g.rit.edu>
2018-07-26 13:40:04 -0400

    add the basic structure for extension

---
+++ popup.js
@@ -0,0 +1,32 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+let changeColor = document.getElementById('changeColor');
+
+let changeBlue = document.getElementById('changeBlue');
+
+chrome.storage.sync.get('color', function(data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
+});
+
+changeColor.onclick = function(element) {
+  let color = element.target.value;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  });
+};
+
+changeBlue.onclick = function(element) {
+	let color = #4688f1;
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			{code: 'document.body.style.backgroundColor = "' +blue+'";'});
+	});
+};
